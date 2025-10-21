a8dde64fd5fcbc191d150ee7c33c4cac9d6bce38
joaomessias2018 <joao.messias@mt.estudante.senai.br>
2025-10-21 19:47:07 -0400

    atualizacao de projeto

---
+++ app/(tabs)/centavo.tsx
@@ -0,0 +1,57 @@
+
+import { Image } from 'expo-image';
+import { StyleSheet, Text, View } from "react-native";
+
+const blurhash =
+  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+
+export default function centavo(){
+  return(
+
+    
+    <View style={styles.container}>
+      <Text> Ola Mundo Animal</Text>
+      
+      <Image
+        style={styles.image}
+        source={require('@/images/images.jpg')}
+        placeholder={{ blurhash }}
+        contentFit="cover"
+        transition={1000}
+      />
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  container:{
+    flex:1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#0553',
+  },
+});
+/* import { StyleSheet, Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}); */
---
+++ app/(tabs)/central.tsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
+
+import { SafeAreaView } from 'react-native';
+import { supabase } from '../../src/supabaseClients';
+
+export default function App(){
+
+    const [nome, setNome] = useState('');
+    const [idade, setIdade] = useState('');
+    const [msg,setMsg] =useState('');
+    
+    const dados = {
+        nome: nome,
+        idade: idade,
+        msg: msg
+    }
+
+    const enviarDados = async () => {
+        const {data, error} = await supabase
+            .from('contato')
+            .insert([{ nome: nome.trim(), idade: idade.trim(), msg: msg.trim() }]);
+    }
+
+    return(
+        
+        <SafeAreaView style={style.SafeArea}>
+            <Text style={style.Text}>Falae</Text>
+            <TextInput
+                style={style.TextInput}
+                placeholder='Digite seu nome'
+                value={nome}
+                onChangeText={setNome}
+            />
+            <Button title = 'Gravar'/>
+            
+            <TextInput
+                style={style.TextInput}
+                placeholder='Digite sua idade'
+                value={idade}
+                onChangeText={setIdade}
+            />
+            <Button title = 'Gravar'/>
+            
+            <TextInput
+                style={style.TextInput}
+                placeholder='Digite a mensagem aqui'
+                value={msg}
+                onChangeText={setMsg}
+            />
+            <Button title = 'Gravar'/>
+
+            <TouchableOpacity style={style.Touchable} onPress={enviarDados}>
+                Cadastrar
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
+}
+
+const style = StyleSheet.create({
+    
+    SafeArea:{
+        display: 'flex',
+        lineHeight: 40
+    },
+
+    TextInput:{
+        width: '30%',
+        height: 45,
+        backgroundColor:'#fff',
+        color: '#000',
+        padding: 5,
+        marginBottom: 10,
+        textAlign: 'center'
+    },
+    
+    Text:{
+        height: 50,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        color: '#fff',
+        marginVertical: 10
+    },
+
+    Touchable:{
+        backgroundColor: '#aaa',
+        borderRadius: 10,
+        width: '30%',
+        textAlign: 'center',
+        margin: 'auto',
+        marginVertical: 25
+    }
+})
