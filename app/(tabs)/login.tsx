8b2e6693e31efeea35a9e000e66a9c53c06dad77
joaomessias2018 <joao.messias@mt.estudante.senai.br>
2025-10-21 21:21:31 -0400

    first commit

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,11 @@
+import { View, StyleSheet, Text } from "react-native";
+
+export default function login(){
+    return(
+        <View>
+            <Text>
+                Só Alegria
+            </Text>
+        </View>
+    )
+}
